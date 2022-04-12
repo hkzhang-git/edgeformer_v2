6524be99f898338dcd41e54c6881332ef8f4e5f0
yangtao2019yt <yangtao2019yt@outlook.com>
2022-04-12 15:57:13 +0800

    gcc_module added, but not tested yet

--- models/convnext_gcc.py
+++ models/convnext_gcc.py
@@ -1,82 +1,11 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
 
-# waiting for implement! 2020.04.11
-# 对于后两个stage，如:depth[2]=9, depth[3]=3，则分别将9个中的3个和3个中的1个替换为gcc_Block
-class gcc_Block(nn.Module):
-    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
-        super().__init__()
-        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
-        self.norm = LayerNorm(dim, eps=1e-6)
-        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
-        self.act = nn.GELU()
-        self.pwconv2 = nn.Linear(4 * dim, dim)
-        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
-                                    requires_grad=True) if layer_scale_init_value > 0 else None
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-
-    def forward(self, x):
-        input = x
-        x = self.dwconv(x)
-        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
-        x = self.norm(x)
-        x = self.pwconv1(x)
-        x = self.act(x)
-        x = self.pwconv2(x)
-        if self.gamma is not None:
-            x = self.gamma * x
-        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
-
-        x = input + self.drop_path(x)
-        return x
-
-class Block(nn.Module):
-    r""" ConvNeXt Block. There are two equivalent implementations:
-    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
-    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
-    We use (2) as we find it slightly faster in PyTorch
-    
-    Args:
-        dim (int): Number of input channels.
-        drop_path (float): Stochastic depth rate. Default: 0.0
-        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-    """
-    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
-        super().__init__()
-        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
-        self.norm = LayerNorm(dim, eps=1e-6)
-        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
-        self.act = nn.GELU()
-        self.pwconv2 = nn.Linear(4 * dim, dim)
-        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
-                                    requires_grad=True) if layer_scale_init_value > 0 else None
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-
-    def forward(self, x):
-        input = x
-        x = self.dwconv(x)
-        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
-        x = self.norm(x)
-        x = self.pwconv1(x)
-        x = self.act(x)
-        x = self.pwconv2(x)
-        if self.gamma is not None:
-            x = self.gamma * x
-        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
-
-        x = input + self.drop_path(x)
-        return x
+from convnext import Block
+from gcc_modules import gcc_Block
 
 class ConvNeXt_gcc(nn.Module):
     def __init__(self, in_chans=3, num_classes=1000, 
@@ -102,10 +31,18 @@ class ConvNeXt_gcc(nn.Module):
         dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
         cur = 0
         for i in range(4):
-            stage = nn.Sequential(
-                *[Block(dim=dims[i], drop_path=dp_rates[cur + j], 
-                layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
-            )
+            if i < 2:   # for stage 0 and 1, no gcc
+                stage = nn.Sequential(*[
+                    Block(dim=dims[i], drop_path=dp_rates[cur + j], layer_scale_init_value=layer_scale_init_value) \
+                    for j in range(depths[i])
+                ])
+            else:       # for stage 2 and 3, gcc modules is used
+                stage = nn.Sequential(*[
+                    Block(dim=dims[i], drop_path=dp_rates[cur + j], layer_scale_init_value=layer_scale_init_value) \
+                    if j+1>(depths[i]-depths[i]//3) else \
+                    gcc_Block(dim=dims[i], drop_path=dp_rates[cur + j], layer_scale_init_value=layer_scale_init_value) \
+                    for j in range(depths[i]) # here we use gcc in the last 1/3 blocks
+                ])                            # e.g., j+1=7 > 9-9//3=6, so block 678 is gcc_block, where block 0~5 is normal
             self.stages.append(stage)
             cur += depths[i]
 
@@ -163,34 +100,34 @@ def convnext_gcc_tiny(pretrained=False,in_22k=False, **kwargs):
     # model = ConvNeXt_gcc(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
     model = ConvNeXt_gcc(depths=[3, 3, 9, 3], dims=[48, 96, 192, 384], **kwargs)
     # model = ConvNeXt_gcc(depths=[3, 3, 9, 3], dims=[24, 48, 96, 192], **kwargs)
-    if pretrained:
+    if pretrained or in_22k:
         raise AttributeError("no pretrained model")
     return model
 
 @register_model
 def convnext_gcc_small(pretrained=False,in_22k=False, **kwargs):
     model = ConvNeXt_gcc(depths=[3, 3, 27, 3], dims=[96, 192, 384, 768], **kwargs)
-    if pretrained:
+    if pretrained or in_22k:
         raise AttributeError("no pretrained model")
     return model
 
 @register_model
 def convnext_gcc_base(pretrained=False, in_22k=False, **kwargs):
     model = ConvNeXt_gcc(depths=[3, 3, 27, 3], dims=[128, 256, 512, 1024], **kwargs)
-    if pretrained:
+    if pretrained or in_22k:
         raise AttributeError("no pretrained model")
     return model
 
 @register_model
 def convnext_gcc_large(pretrained=False, in_22k=False, **kwargs):
     model = ConvNeXt_gcc(depths=[3, 3, 27, 3], dims=[192, 384, 768, 1536], **kwargs)
-    if pretrained:
+    if pretrained or in_22k:
         raise AttributeError("no pretrained model")
     return model
 
 @register_model
 def convnext_gcc_xlarge(pretrained=False, in_22k=False, **kwargs):
     model = ConvNeXt_gcc(depths=[3, 3, 27, 3], dims=[256, 512, 1024, 2048], **kwargs)
-    if pretrained:
+    if pretrained or in_22k:
         raise AttributeError("no pretrained model")
     return model
---
+++ models/gcc_modules.py
@@ -0,0 +1,183 @@
+from turtle import forward
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# channel wise attention
+class CA_layer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(CA_layer, self).__init__()
+        # global average pooling
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(channel, channel//reduction, kernel_size=(1, 1), bias=False),
+            nn.BatchNorm2d(channel//reduction),
+            nn.Hardswish(),
+            nn.Conv2d(channel//reduction, channel, kernel_size=(1, 1), bias=False),
+            nn.BatchNorm2d(channel),
+            nn.Hardsigmoid()
+        )
+
+    def forward(self, x):
+        y = self.fc(self.gap(x))
+        return x*y.expand_as(x)
+
+class channel_pooling(nn.Module):
+    def forward(self,x):
+        max_res = torch.mean(x, dim=1, keepdim=True)
+        avg_res = torch.mean(x, dim=1, keepdim=True)
+        return torch.cat((max_res, avg_res), dim=1)
+
+class gcc_Block(nn.Module):
+    def __init__(self,
+        dim,
+        drop_path=0.,
+        layer_scale_init_value=1e-6,
+        # gcc options
+        meta_kernel_size=32,
+        instance_kernel_method='crop', # YangTao: only 'interpolation_bilinear' mode now
+        use_pe=True,
+        mid_mix=True,
+        bias=True,
+        ffn_dim=2,
+        ffn_dropout=0.0,
+        dropout=0.1
+        ):
+        super(gcc_Block, self).__init__()
+        
+        # record options
+        self.dim = dim
+        self.instance_kernel_method = instance_kernel_method
+        self.use_pe = use_pe
+        self.mid_mix = mid_mix
+
+        # spatial part
+        self.pre_Norm_1 = nn.BatchNorm2d(num_features=dim)
+        self.pre_Norm_2 = nn.BatchNorm2d(num_features=dim)
+
+        self.meta_kernel_1_H = nn.Conv2d(dim, dim, (meta_kernel_size, 1), groups=dim).weight
+        self.meta_kernel_1_W = nn.Conv2d(dim, dim, (1, meta_kernel_size), groups=dim).weight
+        self.meta_kernel_2_H = nn.Conv2d(dim, dim, (meta_kernel_size, 1), groups=dim).weight
+        self.meta_kernel_2_W = nn.Conv2d(dim, dim, (1, meta_kernel_size), groups=dim).weight
+
+        self.meta_1_H_bias = nn.Parameter(torch.randn(dim)) if bias else None
+        self.meta_1_W_bias = nn.Parameter(torch.randn(dim)) if bias else None
+        self.meta_2_H_bias = nn.Parameter(torch.randn(dim)) if bias else None
+        self.meta_2_W_bias = nn.Parameter(torch.randn(dim)) if bias else None
+
+        self.meta_pe_1_H = nn.Parameter(torch.randn(1, dim, meta_kernel_size, 1)) if use_pe else None
+        self.meta_pe_1_W = nn.Parameter(torch.randn(1, dim, 1, meta_kernel_size)) if use_pe else None
+        self.meta_pe_2_H = nn.Parameter(torch.randn(1, dim, meta_kernel_size, 1)) if use_pe else None
+        self.meta_pe_2_W = nn.Parameter(torch.randn(1, dim, 1, meta_kernel_size)) if use_pe else None
+
+        self.mixer = nn.ChannelShuffle(groups=2) if mid_mix else None
+
+        # channel part
+        self.ffn = nn.Sequential(
+            nn.BatchNorm2d(num_features=2*dim),
+            nn.Conv2d(2*dim, ffn_dim, kernel_size=(1, 1), bias=True),
+            nn.Hardswish(),
+            nn.Dropout(p=ffn_dropout),  # using nn.Dropout
+            nn.Conv2d(ffn_dim, 2*dim, kernel_size=(1, 1), bias=True),
+            nn.Dropout(p=dropout)       # using nn.Dropout
+        )
+        self.ca = CA_layer(channel=2*dim)
+
+    def get_instance_kernel(self, instance_kernel_size):
+        # YangTao: only 'interpolation_bilinear' mode now
+        H_shape = [instance_kernel_size, 1]
+        W_shape = [1, instance_kernel_size]
+        return  F.interpolate(self.meta_kernel_1_H, H_shape, mode='bilinear', align_corners=True), \
+                F.interpolate(self.meta_kernel_1_W, W_shape, mode='bilinear', align_corners=True), \
+                F.interpolate(self.meta_kernel_2_H, H_shape, mode='bilinear', align_corners=True), \
+                F.interpolate(self.meta_kernel_2_W, W_shape, mode='bilinear', align_corners=True),
+
+    def get_instance_pe(self, instance_kernel_size):
+        # YangTao: only 'interpolation_bilinear' mode now
+        return  F.interpolate(self.meta_pe_1_H, [instance_kernel_size, 1], mode='bilinear', align_corners=True)\
+                    .expand(1, self.dim, instance_kernel_size, instance_kernel_size), \
+                F.interpolate(self.meta_pe_1_W, [1, instance_kernel_size], mode='bilinear', align_corners=True)\
+                    .expand(1, self.dim, instance_kernel_size, instance_kernel_size), \
+                F.interpolate(self.meta_pe_2_H, [instance_kernel_size, 1], mode='bilinear', align_corners=True)\
+                    .expand(1, self.dim, instance_kernel_size, instance_kernel_size), \
+                F.interpolate(self.meta_pe_2_W, [1, instance_kernel_size], mode='bilinear', align_corners=True)\
+                    .expand(1, self.dim, instance_kernel_size, instance_kernel_size)
+
+    def forward(self, x):
+        x_1, x_2 = torch.chunk(x, 2, 1)
+        x_1_res, x_2_res = x_1, x_2
+        _, _, f_s, _ = x_1.shape
+
+        K_1_H, K_1_W, K_2_H, K_2_W = self.get_instance_kernel(f_s)
+        if self.use_pe:
+            pe_1_H, pe_1_W, pe_2_H, pe_2_W = self.get_instance_pe(f_s)
+
+        # =====================Spatial Part========================
+        # pe
+        if self.use_pe:
+            x_1, x_2 = x_1 + pe_1_H, x_2 + pe_1_W
+        # pre norm
+        x_1, x_2 = self.pre_Norm_1(x_1), self.pre_Norm_2(x_2)
+        # stage 1
+        x_1_1 = F.conv2d(torch.cat((x_1, x_1[:, :, :-1, :]), dim=2),
+            weight=K_1_H, bias=self.meta_1_H_bias, padding=0, groups=self.dim)
+        x_2_1 = F.conv2d(torch.cat((x_2, x_2[:, :, :, :-1]), dim=3),
+            weight=K_1_W, bias=self.meta_1_W_bias, padding=0, groups=self.dim)
+        if self.mid_mix:
+            mid_rep = torch.cat((x_1_1, x_2_1), dim=1)
+            x_1_1, x_2_1 = torch.chunk(self.mixer(mid_rep), chunks=2, dim=1)
+
+        # pe
+        if self.use_pe:
+            x_1_1, x_2_1 = x_1_1 + pe_2_W, x_2_1 + pe_2_H
+        # stage 2
+        x_1_2 = F.conv2d(torch.cat((x_1_1, x_1_1[:, :, :, :-1]), dim=3),
+            weight=K_2_W, bias=self.meta_2_W_bias, padding=0, groups=self.dim)
+        x_2_2 = F.conv2d(torch.cat((x_2_1, x_2_1[:, :, :-1, :]), dim=2), 
+            weight=K_2_H, bias=self.meta_2_H_bias, padding=0, groups=self.dim)
+
+        # residual
+        x_1, x_2 = x_1_res + x_1_2, x_2_res + x_2_2
+
+        # =====================Channel Part========================
+        x_ffn = torch.cat((x_1, x_2), dim=1)
+        x_ffn = x_ffn + self.ca(self.ffn(x_ffn))
+
+        return x_ffn
+
+class Block(nn.Module):
+    r""" ConvNeXt Block. There are two equivalent implementations:
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
+    We use (2) as we find it slightly faster in PyTorch
+    
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), 
+                                    requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
